f3c2a9dc1c0fdc3f39cbd578e59b0c7f03662925
Filip Kaczmarek <filipkaczmarek@macbook-air-2.home>
2021-01-16 16:33:43 +0100

    JavaScript basic functionality

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-let rootContainer = null;
+// App state
 
-let filter = 'ALL'; // one of ALL, DONE, NOT-DONE
-let sort = "ASCENDING" // ASCENDING OR DESCENDING
+let mainContainer = null
 
-let searchPhrase = '';
-let serachInputIsFocused = false;
-let newToDoName = '';
-let newToDoInputIsFocused = false;
+let filter = 'ALL' // one of ALL, DONE, NOT-DONE
+let sort = 'ASCENDING' // ASCENDING or DESCENDING
+
+let searchPhrase = ''
+let searchInputIsFocused = false
+let newToDoName = ''
+let newToDoInputIsFocused = false
 
 let tasks = [
     {
-        name: 'Do the dishes',
+        name: 'Wynieś śmieci',
+        isCompleted: true,
+    },
+    {
+        name: 'Zmyj naczynia',
         isCompleted: false,
     }
-];
+]
 
-// -------
+// Generic / helper functions
 
-const focus = function(condition, element){
-    if(condition) {
+const focus = function (condition, element) {
+    if (condition) {
         setTimeout(
             function () {
                 element.focus()
@@ -35,10 +41,10 @@ const appendArray = function (array, container) {
 }
 
 const renderInput = function (onChange, focusCondition, className) {
-    const input = document.createElement('input');
-    input.className = className;
+    const input = document.createElement('input')
+    input.className = className
 
-    input.value = newToDoName;
+    input.value = newToDoName
 
     input.addEventListener('input', onChange)
 
@@ -47,118 +53,172 @@ const renderInput = function (onChange, focusCondition, className) {
     return input
 }
 
-// -------
+const renderButton = function (label, onClick, className) {
+    const button = document.createElement('button')
+    button.className = className
+
+    if (onClick) {
+        button.addEventListener('click', onClick)
+    }
+
+    button.innerText = label
+
+    return button
+}
+
+// State changing functions
 
 const onNewToDoNameChange = function (event) {
-    newToDoInputIsFocused = true;
-    newToDoName = event.target.value;
+    newToDoInputIsFocused = true
+    newToDoName = event.target.value
     update()
 }
 
-const onNewToDoButtonSubmit = function (event) {
-    event.preventDefault();
-
-    newToDoName 
+const onNewToDoSubmit = function (event) {
+    event.preventDefault()
 
     tasks = tasks.concat({
         name: newToDoName,
-        isCompleted: fasle
+        isCompleted: false,
     })
 
     newToDoName = ''
+
     update()
 }
 
-// -------
+const onTaskCompleteToggle = function (indexToToggle) {
+    tasks = tasks.map(function (task, index) {
+        if (index !== indexToToggle) return task
 
-const renderTask = function (task) {
-    const container = document.createElement('li');
-    container.classList = 'todo-list__list-item';
+        return {
+            name: task.name,
+            isCompleted: !task.isCompleted,
+        }
+    })
 
+    update()
+}
+
+const onTaskDelete = function (indexToDelete) {
+    tasks = tasks.filter(function (task, index) {
+        return index !== indexToDelete
+    })
+
+    update()
+}
+
+// Rendering
+
+const renderTask = function (task, onTaskToggle, onDelete) {
+    const container = document.createElement('li')
+    const wrapper = document.createElement('div')
+    const textContainer = document.createElement('span')
+ 
+    container.className = 'todo-list__list-item'
+    wrapper.className = 'todo-list__list-item-wrapper'
+    textContainer.className = 'todo-list__list-item-text-container'
     if (task.isCompleted) {
-        container.className = container.className + ' todo-list__list-item--completed';
+        container.className = container.className + ' todo-list__list-item--completed'
     }
 
-    container.innerText = task.name;
+    const deleteButton = renderButton(
+        'X',
+        onDelete,
+        'todo-list__button todo-list__button--delete'
+    )
+    container.addEventListener('click', onTaskToggle)
 
+    const text = document.createTextNode(task.name)
+
+    textContainer.appendChild(text)
+    wrapper.appendChild(textContainer)
+    wrapper.appendChild(deleteButton)
+    container.appendChild(wrapper)
 
     return container
 }
 
 const renderTasksList = function (tasks) {
-    const container = document.createElement('ol');
-    container.classList = 'todo-list__list';
+    const container = document.createElement('ol')
+    container.className = 'todo-list__list'
 
-    const tasksElements = tasks.map((task) => {
-        return renderTask(task)
+    const tasksElements = tasks.map(function (task, index) {
+        return renderTask(
+            task,
+            function () { onTaskCompleteToggle(index) },
+            function () { onTaskDelete(index) },
+        )
     })
+
     appendArray(tasksElements, container)
 
     return container
 }
 
 const renderNewTaskButton = function (label) {
-    const button = document.createElement('button');
-    button.className = "todo-list__button";
-
-    button.innerText = label;
-
-    return button
+    return renderButton(label, null, 'todo-list__button')
 }
 
-const renderNewTaskInput = function(){
+const renderNewTaskInput = function () {
     return renderInput(
         onNewToDoNameChange,
         newToDoInputIsFocused,
-        'todo-list__input');
+        'todo-list__input'
+    )
 }
 
 const renderNewTaskForm = function () {
-    const container = document.createElement('form');
-    container.className = 'todo-list__form';
+    const container = document.createElement('form')
+    container.className = 'todo-list__form'
 
-    container.addEventListener('submit', onNewToDoButtonSubmit)
+    container.addEventListener('submit', onNewToDoSubmit)
 
-    const inputElement = renderNewTaskInput();
-    const buttonElement = renderNewTaskButton('ADD');
+    const inputElement = renderNewTaskInput()
+    const buttonElement = renderNewTaskButton('ADD')
 
-    container.appendChild(inputElement);
-    container.appendChild(buttonElement);
+    container.appendChild(inputElement)
+    container.appendChild(buttonElement)
 
     return container
 }
 
 const render = function () {
-    const container = document.createElement('div');
-    container.classList = 'todo-list';
+    const container = document.createElement('div')
+    container.className = 'todo-list'
 
-    const newTaskFromElement = renderNewTaskForm()
+    const newTaskFormElement = renderNewTaskForm()
     const taskListElement = renderTasksList(tasks)
 
-    container.appendChild(newTaskFromElement)
+    container.appendChild(newTaskFormElement)
     container.appendChild(taskListElement)
 
     return container
 }
 
 const update = function () {
-    rootContainer.innerHTML = ''
-
-    rootContainer.appendChild(render())
-}
-
-const init = function (selector) {
-    const container = document.querySelector(selector)
-
-    if (!container) {
-        console.log('Container do not exist')
-    }
-
-    rootContainer = container
+    mainContainer.innerHTML = ''
 
     const app = render()
 
-    rootContainer.appendChild(app)
+    mainContainer.appendChild(app)
+}
+
+const init = function (selector) {
+
+    const container = document.querySelector(selector)
+
+    if (!container) {
+        console.log('Container do not exist!')
+        return
+    }
+
+    mainContainer = container
+
+    const app = render()
+
+    mainContainer.appendChild(app)
+
 }
 
 init('.root')
